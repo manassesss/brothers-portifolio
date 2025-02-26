f9f5297305480bc8ed8564c8f1286ca43c9c0799
manasses.santos <manasses.santos@maida.health>
2025-02-26 17:54:17 -0300

    showcase done

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,17 @@ import Image from "next/image";
 import Header from "@/components/Header";
 import HomeSession from "@/components/HomeSession";
 import AboutSession from "@/components/AboutSession";
+import ShowCaseSession from "@/components/ShowcaseSession";
+import DetailSession from "@/components/DetailSession";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <HomeSession id='#home'/>
-      <AboutSession id='#about'/>
+      <HomeSession id='home'/>
+      <AboutSession id='about'/>
+      <ShowCaseSession id='showcase'/>
+      <DetailSession/>
     </div>
   );
 }
