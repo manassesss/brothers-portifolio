94ae4e413df2f58b03cdce435ea0cbc8752d3397
manasses.santos <manasses.santos@maida.health>
2025-02-26 18:26:35 -0300

    footer done instagram session added

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,25 @@ import HomeSession from "@/components/HomeSession";
 import AboutSession from "@/components/AboutSession";
 import ShowCaseSession from "@/components/ShowcaseSession";
 import DetailSession from "@/components/DetailSession";
+import Instagram from "@/components/Instagram";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <HomeSession id='home'/>
-      <AboutSession id='about'/>
-      <ShowCaseSession id='showcase'/>
+      <section  id='home'>
+        <HomeSession/>
+      </section>
+      <section  id='about'>
+      <AboutSession/>
+      </section>
+      <section id='showcase'>
+        <ShowCaseSession />
+      </section>
       <DetailSession/>
+      <Instagram/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Instagram.js
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+
+export default function Instagram () {
+  return (
+    <div className="w-full bg-white flex flex-row justify-center justify-items-center items-center">
+        <script src="https://static.elfsight.com/platform/platform.js" async/>
+        <div className="elfsight-app-0d2448a8-d682-4f15-bfbf-0babce056093" data-elfsight-app-lazy></div>
+    </div>
+  );
+};
